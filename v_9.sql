81fcf51b46b19f43a208ad8bbcac25d5206765dc
AliKvitek <85629752+AliKvitek@users.noreply.github.com>
2021-06-23 20:14:59 +0200

    Update v_9.sql

--- v_9.sql
+++ v_9.sql
@@ -1,12 +1,12 @@
 CREATE VIEW v_9 AS
-WITH rozdil as 
+WITH rozdil AS 
  (SELECT country,
 		life_expectancy as life_expectancy_1965
 	FROM life_expectancy
 	WHERE `year` = 1965)
 SELECT 
 	le.country,
-	le.life_expectancy as life_expectancy_2005,
+	le.life_expectancy AS life_expectancy_2005,
 	r.life_expectancy_1965,
 	ROUND(le.life_expectancy - r.life_expectancy_1965,1) AS age_difference1965_2005
 FROM life_expectancy le 
